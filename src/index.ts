cebf78f77d719a10742fdb4c7ed74142db9aeacb
Jonas de Luna Skulberg <jonasdls@gmail.com>
2023-06-27 11:47:23 +0200

    Implement line component

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import Grid from "./Components/Grid";
+<<<<<<< HEAD
 import Point from "./Components/Point";
+=======
+import Line from "./Components/Line";
+>>>>>>> 464899e (Implement line component)
 import Graphica from "./Graphica";
 
 const graphica = new Graphica(document.body);
 
 const grid = new Grid();
 graphica.add(grid);
+<<<<<<< HEAD
 const point = new Point();
 graphica.add(point);
 
+=======
+graphica.add(new Line([0, 0], [10, -10]));
+>>>>>>> 464899e (Implement line component)
 graphica.run();
