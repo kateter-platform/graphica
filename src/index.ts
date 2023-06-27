335ef83e8e13aeef2f7144597bf1a054d9c0962f
Klara Wüstenberg <klarawust@gmail.com>
2023-06-27 12:06:08 +0200

    created Point-Component

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import Grid from "./Components/Grid";
+import Point from "./Components/Point";
 import Graphica from "./Graphica";
 
 const graphica = new Graphica(document.body);
 
 const grid = new Grid();
-graphica.add(grid)
+graphica.add(grid);
+const point = new Point();
+graphica.add(point);
 
 graphica.run();
