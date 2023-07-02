a4051ea615264bee73d78be52a2b14e0c25b5ab6
Mattias Tofte <mattiastofte@gmail.com>
2023-07-02 14:22:50 +0200

    Jost font for text component

--- webpack.config.ts
+++ webpack.config.ts
@@ -25,6 +25,18 @@ const config: Configuration = {
         },
         exclude: /node_modules/,
       },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
     ],
   },
   resolve: {
