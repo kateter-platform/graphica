63a55a88f6e76bb61a5c5e65c9aadd3754845882
Klara <102354684+klarawust@users.noreply.github.com>
2023-07-04 14:13:35 +0200

    Create arc2.0 (#20)

---
+++ src/Components/Arc.ts
@@ -0,0 +1,92 @@
+import { ArcCurve, CircleGeometry, MeshBasicMaterial, Vector2 } from "three";
+import { Line2, LineGeometry, LineMaterial } from "three-fatline";
+import Line from "./Line";
+import Text from "./Text";
+import { Component } from "./interfaces";
+
+type ArcOptions = {
+  pointA: Vector2;
+  pointB: Vector2;
+  pointC: Vector2;
+  radius: number;
+};
+
+class Arc extends Component {
+  constructor({ pointA, pointB, pointC, radius = 20 }: ArcOptions) {
+    super();
+    //finne linjene i vinkelen og den minste vinkelen mellom de
+    const vectorBtoA = pointA.clone().sub(pointB.clone());
+    const vectorBtoC = pointC.clone().sub(pointB.clone());
+    const angle = vectorBtoA.angleTo(vectorBtoC);
+    //finner hvilken vinkel som er riktig
+    const angle1 =
+      (Math.atan2(vectorBtoA.y, vectorBtoA.x) + Math.PI * 2) % (Math.PI * 2);
+    const angle2 =
+      (Math.atan2(vectorBtoC.y, vectorBtoC.x) + Math.PI * 2) % (Math.PI * 2);
+
+    let startAngle = angle2; // Starting angle of the arc in radians
+
+    if (
+      (angle1 - angle2 + Math.PI * 2) % (2 * Math.PI) >
+      (angle2 - angle1 + Math.PI * 2) % (2 * Math.PI)
+    ) {
+      startAngle = angle1;
+    }
+    const endAngle = startAngle + angle; // Ending angle of the arc in radians
+    const clockwise = false; // Whether the arc is drawn in a clockwise direction
+
+    //Create Arc-curve
+    const arcCurve = new ArcCurve(
+      0,
+      0,
+      radius,
+      startAngle,
+      endAngle,
+      clockwise
+    );
+    //generate points on ArcCurve
+    const points = arcCurve.getPoints(50);
+
+    //punktene arcen krysser linjene
+    const krysningAB = vectorBtoA.clone().normalize().multiplyScalar(radius);
+    const krysningCB = vectorBtoC.clone().normalize().multiplyScalar(radius);
+
+    // Create circle-geometry -- lager fyllet i vinkelen
+    this.geometry = new CircleGeometry(radius, 32, startAngle, angle);
+    this.material = new MeshBasicMaterial({ color: "#FAA307" });
+
+    //setter posisjonen til å være fra punktB
+    this.position.set(pointB.x, pointB.y, 0);
+
+    //lage outline og tekst
+    const vinkelTekst = new Text(
+      (Math.round((angle * 180) / Math.PI) + " °").toString(),
+      { fontSize: 18, anchorY: "middle", anchorX: "left", position: [15, 0] }
+    );
+    const linje1 = new Line(krysningAB, [0, 0], {
+      lineWidth: 4,
+      color: 0x080007,
+    });
+    const linje2 = new Line([0, 0], krysningCB, {
+      lineWidth: 4,
+      color: 0x080007,
+    });
+    const tempLine = new LineGeometry();
+    tempLine.setPositions(points.flatMap((v) => [v.x, v.y, 3]));
+    const linje3 = new Line2(
+      tempLine,
+      new LineMaterial({
+        color: 0x080007,
+        linewidth: 4,
+        resolution: new Vector2(window.innerWidth, window.innerHeight),
+      })
+    );
+
+    //legger til outline og tekst
+    this.add(vinkelTekst);
+    this.add(linje1);
+    this.add(linje2);
+    this.add(linje3);
+  }
+}
+export default Arc;
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,36 @@
+import { Vector2 } from "three";
+import Arcc from "./Components/Arc";
 import Grid from "./Components/Grid";
-import Line from "./Components/Line";
-import Plot from "./Components/Plot";
 import Point from "./Components/Point";
-import Text from "./Components/Text";
 import Graphica from "./Graphica";
 
 const graphica = new Graphica(document.body);
 
 const grid = new Grid();
-const point = new Point(10, 10, { label: true, draggable: "unrestricted" });
-const line = new Line([0, 0], point, { color: 0x000000, lineWidth: 4 });
-const text = new Text("Hello World this is awesome!", {
-  position: [0, 10],
-  color: "black",
-  fontSize: 10,
-  anchorX: "left",
-  anchorY: "bottom",
+
+const pointB = new Point(70, 60, { draggable: "unrestricted" });
+const pointA = new Point(20, 30, {
+  draggable: "unrestricted",
+  color: "#F4493B",
+});
+const pointC = new Point(40, 20, {
+  draggable: "unrestricted",
+  color: "#F4493B",
+});
+const arc = new Arcc({
+  pointA: new Vector2(20, 30),
+  pointB: new Vector2(70, 60),
+  pointC: new Vector2(40, 20),
+  radius: 30,
 });
 const a = new Plot("sin(x)", {});
 const b = new Line([0, 0], [50, 50], { color: 0x000000 });
 graphica.add(a);
 graphica.add(b);
 graphica.add(grid);
-graphica.add(line);
-graphica.add(point);
-graphica.add(text);
+graphica.add(arc);
+graphica.add(pointA);
+graphica.add(pointB);
+graphica.add(pointC);
 
 graphica.run();
