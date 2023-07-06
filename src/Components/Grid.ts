35678e036da9502165e61efc03f3f7898b3cb43f
Mattias Tofte <mattiastofte@gmail.com>
2023-07-06 14:18:06 +0200

    Add vectors, improve grid, improve font and many small fixes (#29)
    * Add vectors, improve grid, improve font and many small fixes
    
    * Fix linting errors

--- src/Components/Grid.ts
+++ src/Components/Grid.ts
@@ -1,10 +1,14 @@
 import {
   Color,
+  Vector2,
   Vector3,
   PlaneGeometry,
   ShaderMaterial,
   OrthographicCamera,
+  Mesh,
 } from "three";
+import Line from "./Line";
+import Text from "./Text";
 import { Component } from "./interfaces";
 
 const vertexShader = `
@@ -45,21 +49,39 @@ type GridOptions = {
   cellSize?: number;
   pointRadius?: number;
   pointColor?: Color;
+  labels?: boolean;
+  yLabelText?: string;
+  xLabelText?: string;
 };
 
 const defaultGridOptions: GridOptions = {
   cellSize: 50,
   pointRadius: 1.5,
   pointColor: new Color(0xe1e1e1),
+  labels: true,
+  xLabelText: "x",
+  yLabelText: "y",
 };
 
 class Grid extends Component {
   draggable = undefined;
+  private shaderMesh: Mesh;
+  private xAxis: Line;
+  private yAxis: Line;
+  private xLabel: Text;
+  private yLabel: Text;
 
   constructor(options?: GridOptions) {
     super();
 
-    const { cellSize, pointRadius, pointColor } = {
+    const {
+      cellSize,
+      pointRadius,
+      pointColor,
+      labels,
+      xLabelText,
+      yLabelText,
+    } = {
       ...defaultGridOptions,
       ...options,
     };
@@ -82,16 +104,58 @@ class Grid extends Component {
       transparent: true,
     });
 
-    this.geometry = gridGeometry;
-    this.material = gridMaterial;
+    this.shaderMesh = new Mesh(gridGeometry, gridMaterial);
+    this.xAxis = new Line(
+      new Vector2(-window.innerWidth, 0),
+      new Vector2(window.innerWidth - 1000, 0),
+      { arrowhead: true, lineWidth: 3 }
+    );
+    this.yAxis = new Line(
+      new Vector2(0, -window.innerHeight),
+      new Vector2(0, window.innerHeight - 1000),
+      { arrowhead: true, lineWidth: 3 }
+    );
+    this.xLabel = new Text(xLabelText, { color: "#000000", fontSize: 22 });
+    this.yLabel = new Text(yLabelText, { color: "#000000", fontSize: 22 });
+    if (labels) {
+      this.add(this.xLabel, this.yLabel);
+    }
+
+    this.add(this.shaderMesh, this.xAxis, this.yAxis);
   }
 
   update(camera: OrthographicCamera) {
-    (this.material as ShaderMaterial).uniforms.zoomLevel.value = camera.zoom;
-    (this.material as ShaderMaterial).uniforms.offset.value = camera.position;
+    (this.shaderMesh.material as ShaderMaterial).uniforms.zoomLevel.value =
+      camera.zoom;
+    (this.shaderMesh.material as ShaderMaterial).uniforms.offset.value =
+      camera.position;
 
-    this.position.set(camera.position.x, camera.position.y, -1);
-    this.scale.set(1 / camera.zoom, 1 / camera.zoom, 1);
+    this.shaderMesh.position.set(camera.position.x, camera.position.y, -1);
+    this.shaderMesh.scale.set(1 / camera.zoom, 1 / camera.zoom, 1);
+    this.xAxis.start = new Vector2(
+      camera.position.x - window.innerWidth / camera.zoom,
+      0
+    );
+    this.xAxis.end = new Vector2(
+      camera.position.x + (window.innerWidth - 780) / camera.zoom,
+      0
+    );
+    this.yAxis.start = new Vector2(
+      0,
+      camera.position.y - window.innerHeight / camera.zoom
+    );
+    this.yAxis.end = new Vector2(
+      0,
+      camera.position.y + (window.innerHeight - 400) / camera.zoom
+    );
+    this.xLabel.position.setX(
+      camera.position.x + (window.innerWidth - 792) / camera.zoom
+    );
+    this.xLabel.position.setY(15 / camera.zoom);
+    this.yLabel.position.setY(
+      camera.position.y + (window.innerHeight - 420) / camera.zoom
+    );
+    this.yLabel.position.setX(23 / camera.zoom);
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,9 @@
-import { Vector2 } from "three";
 import Grid from "./Components/Grid";
-import Latex from "./Components/Latex";
-import Plot from "./Components/Plot";
-import Polygon from "./Components/Shape";
-import Slider from "./Components/Slider";
-import { Component } from "./Components/interfaces";
 import Graphica from "./Graphica";
 
 const graphica = new Graphica(document.body);
 
-const grid = new Grid();
+const grid = new Grid({ yLabelText: "y", xLabelText: "x" });
 
-const p = new Plot("cos(x)* sin(x) + sin(x)");
-const s = new Slider({ maxValue: 500, minValue: 1 });
-let a = new Component();
-
-const l = new Latex(
-  " \\sum_{i=1}^{n} [\\sin(x_i)\\cdot\\cos(x_i)+\\sin(x_i)] \\Delta x",
-  {
-    position: [-10, 5],
-  }
-);
-
-s.addObserver((v) => {
-  graphica.remove(a);
-  a = new Component();
-  const range = 20;
-  const stepSize = range / v;
-  for (let i = 0; i < range; i += stepSize) {
-    a.add(
-      new Polygon(
-        [
-          new Vector2(i, 0),
-          new Vector2(i + stepSize, 0),
-          new Vector2(i + stepSize, Math.sin(i) * Math.cos(i) + Math.sin(i)),
-          new Vector2(i, Math.sin(i) * Math.cos(i) + Math.sin(i)),
-        ],
-        { fill: true }
-      )
-    );
-  }
-  graphica.add(a);
-});
-
-graphica.add(l);
-graphica.addGui(s);
-graphica.add(p);
 graphica.add(grid);
 graphica.run();
