2c81dfed501edded219d0840c6152870059d2598
Mattias Tofte <mattiastofte@gmail.com>
2023-07-06 14:14:59 +0200

    Fix linting errors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
-import { Vector2 } from "three";
 import Grid from "./Components/Grid";
-import Latex from "./Components/Latex";
-import Plot from "./Components/Plot";
-import Point from "./Components/Point";
-import Vector from "./Components/Vector";
-import Polygon from "./Components/Shape";
-import Slider from "./Components/Slider";
-import { Component } from "./Components/interfaces";
 import Graphica from "./Graphica";
 
 const graphica = new Graphica(document.body);
