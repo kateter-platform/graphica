8d4b1a383c9a5662fa1cb2c668154e5d037165f8
Jonas de Luna Skulberg <jonasdls@gmail.com>
2023-07-10 11:18:09 +0200

    Initial working svgloader, needs improvement as lines are not being rendered

---
+++ src/Components/SVGLoader.ts
@@ -0,0 +1,100 @@
+import {
+  Color,
+  DoubleSide,
+  Group,
+  Mesh,
+  MeshBasicMaterial,
+  Shape,
+  ShapeGeometry,
+  Line,
+  LineBasicMaterial,
+  BufferGeometry,
+  BufferAttribute,
+  Vector2,
+  Box3,
+  Vector3,
+} from "three";
+import { SVGLoader } from "three/examples/jsm/loaders/SVGLoader";
+import { Line2, LineGeometry, LineMaterial } from "three-fatline";
+import { Component } from "./interfaces";
+import { InputPosition } from "./types";
+
+class SVG extends Component {
+  draggable = undefined;
+
+  constructor(url: string, position = new Vector3(0, 0, 0)) {
+    super();
+    const loader = new SVGLoader();
+
+    loader.load(
+      url,
+      (data) => {
+        const paths = data.paths;
+        const group = new Group();
+
+        for (const path of paths) {
+          const fillColor =
+            path.userData?.style.fill !== undefined
+              ? path.userData.style.fill
+              : 0x000000;
+          const strokeColor =
+            path.userData?.style.stroke !== undefined
+              ? path.userData?.style.stroke
+              : 0x000000;
+
+          const strokeWidth =
+            path.userData.style.strokeWidth !== undefined
+              ? path.userData?.style.strokeWidth
+              : 1;
+
+          const shapes = SVGLoader.createShapes(path);
+
+          for (const shape of shapes) {
+            const geometry = new ShapeGeometry(shape);
+
+            // Check if the shape has a fill property
+            if (path.userData?.style.fill !== undefined) {
+              const material = new MeshBasicMaterial({
+                color: fillColor,
+                side: DoubleSide,
+              });
+              const mesh = new Mesh(geometry, material);
+              group.add(mesh);
+            }
+
+            // Check if the shape has a stroke property
+            if (path.userData?.style.stroke !== undefined) {
+              const strokeGeometry = new LineGeometry().setFromPoints(
+                shape.getPoints()
+              );
+              const material = new LineMaterial({
+                color: strokeColor,
+                linewidth: 5,
+                resolution: new Vector2(window.innerWidth, window.innerHeight),
+              });
+              const line = new Line2(strokeGeometry, material);
+              console.log(shape.getPoints());
+              console.log(strokeWidth);
+              group.add(line);
+            }
+          }
+        }
+        group.rotation.set(Math.PI, 0, 0);
+        const boundingBox = new Box3().setFromObject(group);
+        const size = new Vector3();
+        boundingBox.getSize(size);
+        group.position.set(position.x, position.y + size.y, position.z);
+        this.add(group);
+      },
+
+      (error) => {
+        console.log(
+          "An error happened loading the provided SVG. Is the path correct?",
+          error
+        );
+      }
+    );
+  }
+}
+
+export default SVG;
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-export { default as Graphica } from "./Graphica";
-export { default as Arc } from "./Components/Arc";
-export { default as Bracket } from "./Components/Bracket";
-export { default as Button } from "./Components/Button";
-export { default as Circle } from "./Components/Circle";
-export { default as Grid } from "./Components/Grid";
-export { default as InfiniteLine } from "./Components/InfiniteLine";
-export { default as InputField } from "./Components/InputField";
-export { default as Label } from "./Components/Label";
-export { default as Latex } from "./Components/Latex";
-export { default as Line } from "./Components/Line";
-export { default as Plot } from "./Components/Plot";
-export { default as Point } from "./Components/Point";
-export { default as Shape } from "./Components/Shape";
-export { default as Slider } from "./Components/Slider";
-export { default as Text } from "./Components/Text";
-export { default as Vector } from "./Components/Vector";
-export { default as Fraction } from "./Components/Derived/Fraction";
+import Grid from "./Components/Grid";
+import Latex from "./Components/Latex";
+import SVG from "./Components/SVGLoader";
+import Graphica from "./Graphica";
 
-export * as three from "three";
+const graphica = new Graphica();
+
+const grid = new Grid();
+
+const a = new SVG("../public/images/Funksjonsmaskin 2.svg");
+
+const b = new Latex('" \\pi ^ 2 = g "', { position: [800, 250] });
+const c = new Latex("- Kato", { position: [800, 190], fontSize: 35 });
+
+graphica.add(b);
+graphica.add(c);
+
+graphica.add(a);
+graphica.add(grid);
+graphica.run();
