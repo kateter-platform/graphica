92c0045f0bb4f8b91aed1c2dfe23ae9fe6ab2e08
Kristiantvaa <kraa1611@gmail.com>
2024-07-01 11:20:42 +0200

    Structured the code more clearly

--- src/Components/Derived/BarDiagram.ts
+++ src/Components/Derived/BarDiagram.ts
@@ -23,6 +23,8 @@ type BarDiagramOptions = {
   basePosition?: [number, number];
 };
 
+type Position = [[number, number], [number, number]];
+
 class BarDiagram extends Component {
   data: number[];
   labels: string[];
@@ -32,6 +34,16 @@ class BarDiagram extends Component {
   xAxisUnit: string | undefined;
   yAxisUnit: string | undefined;
   basePosition: [number, number] | undefined;
+
+  maxLength: number;
+  maxHeight: number;
+
+  // Extra fields
+  fontSize: number;
+  labelsNextToLinePosition: number;
+  distanceMultiplierBarLabels: number;
+  maxData: number;
+  normalizationFactor: number;
 
   constructor(
     data: number[],
@@ -53,6 +65,18 @@ class BarDiagram extends Component {
     this.yAxisUnit = yAxisUnit ? yAxisUnit : "";
     this.basePosition = options?.basePosition ? options?.basePosition : [0, 0];
 
+    this.maxLength = 36;
+    this.maxHeight = 10;
+
+    // Extra fields
+    this.fontSize = 26;
+    this.distanceMultiplierBarLabels = 0.03;
+    this.labelsNextToLinePosition =
+      -this.fontSize * this.distanceMultiplierBarLabels;
+
+    this.maxData = Math.max(...this.data);
+    this.normalizationFactor = this.maxData / 10;
+
     this.position.set(this.basePosition[0], this.basePosition[1], 0);
     this.createBarDiagram();
   }
@@ -60,108 +84,9 @@ class BarDiagram extends Component {
   createBarDiagram() {
     // Lage en gruppe med bars
 
-    const allBars = new Group();
-    const allLabels = new Group();
-    const lines = new Group();
+    const basePosition = this.makeBars(this.data, this.labels);
 
-    let counter = 0;
-
-    const widthOfDiagram = 15; //Width for bars and spacing (not counting the xLine)
-    const maxLength = 36; //Max length of the xLine
-
-    // Want the spacing to be 2/3 of the width of the bars
-    // maxLength = numOfBars * width + (numOfBars + 1) * spacingBetweenBars
-
-    const numOfBars = this.data.length;
-    // const widthOfBars = 108 / (5 * numOfBars);
-    // const spacingBetweenBars = (2 / 3) * widthOfBars;
-    const widthOfBars = maxLength / ((5 / 3) * numOfBars + 2 / 3);
-    const spacingBetweenBars = (2 / 3) * widthOfBars;
-    let basePosition = 0 + spacingBetweenBars;
-
-    // const widthOfBars = 3;
-    // const spacingBetweenBars = 3;
-
-    const fontSize = 26;
-
-    const distanceMultiplierForBarLabels = 0.03;
-    const labelsNextToLinePos = -fontSize * distanceMultiplierForBarLabels;
-
-    const maxData = Math.max(...this.data);
-    const normalizationFactor = maxData / 10;
-
-    this.data = this.data.map((elem) => {
-      // console.log("ELEM FØR: ", elem)
-      elem = elem / normalizationFactor;
-      //   console.log("ELEM ETTER: ", elem);
-      return elem;
-    });
-
-    // console.log("Normalisert data: ", this.data);
-
-    while (counter < this.data.length) {
-      const height = this.data[counter];
-
-      const bar = new Polygon(
-        [
-          [basePosition, height],
-          [basePosition + widthOfBars, height],
-          [basePosition + widthOfBars, 0],
-          [basePosition, 0],
-        ],
-        { transparent: false, opacity: 1.0 }
-      );
-
-      const label = new Text(this.labels[counter], {
-        fontSize: fontSize,
-        position: [basePosition + widthOfBars / 2, labelsNextToLinePos],
-        anchorX: "center",
-      });
-      const valueOfBar = new Text(
-        "" + this.data[counter] * normalizationFactor,
-        {
-          fontSize: fontSize,
-          position: [basePosition + widthOfBars / 2, height + 0.1],
-          anchorX: "center",
-        }
-      );
-      basePosition += widthOfBars + spacingBetweenBars;
-
-      allBars.add(bar);
-      allLabels.add(label);
-      allLabels.add(valueOfBar);
-      counter++;
-    }
-    console.log(basePosition);
-
-    const xLineEndpoint: [number, number] = [basePosition, 0];
-    const yLineEndpoint: [number, number] = [0, Math.max(...this.data) * 1.25];
     const stringLengthMultiplier = 0.2; //For distributing the yAxisTitle and horizontal line-labels
-
-    const xLine = new Line([0, 0], xLineEndpoint, { arrowhead: true });
-    const yLine = new Line([0, 0], yLineEndpoint, {
-      arrowhead: true,
-    });
-
-    const xAxisTitle = new Text(this.xAxisTitle, {
-      fontSize: fontSize,
-      position: [basePosition / 2, labelsNextToLinePos * 2.5],
-      anchorX: "center",
-    });
-    const yAxisTitle = new Text(this.yAxisTitle, {
-      fontSize: fontSize,
-      position: [
-        labelsNextToLinePos * 2.5 -
-          maxData.toString().length * stringLengthMultiplier,
-        yLineEndpoint[1] / 2,
-      ],
-      anchorX: "center",
-      anchorY: "top",
-    });
-    yAxisTitle.rotateZ(Math.PI / 2);
-
-    allLabels.add(xAxisTitle);
-    allLabels.add(yAxisTitle);
 
     // Horizontal lines
     // const niceInterval = this.calculateNiceInterval(maxData);
@@ -181,64 +106,196 @@ class BarDiagram extends Component {
     //   });
     //   allLabels.add(valueOfValueLine);
     // }
+
+    const [xLineCoord, yLineCoord] = this.addAxes(
+      basePosition,
+      stringLengthMultiplier
+    );
+
+    this.addTitle([basePosition, yLineCoord[1][1]]);
+
+    this.addAxisUnits(xLineCoord, yLineCoord);
     const numOfLines = 5;
-    for (let i = 1; i < numOfLines + 1; i++) {
-      const yCoord = (maxData / normalizationFactor / numOfLines) * i;
-      const valueLine = new Line([0, yCoord], [xLineEndpoint[0], yCoord], {
-        color: 0xaaaaaa,
-      });
-      lines.add(valueLine);
-      const yValue = Math.round(yCoord * normalizationFactor);
-      const valueOfValueLine = new Text("" + yValue, {
-        position: [
-          labelsNextToLinePos -
-            yValue.toString().length * stringLengthMultiplier,
-          yCoord,
+    this.addHorizontalLines(
+      numOfLines,
+      stringLengthMultiplier,
+      xLineCoord,
+      yLineCoord
+    );
+  }
+
+  makeBars(data: number[], labels: string[]): number {
+    const allBars = new Group();
+    const allBarsLabels = new Group();
+
+    let counter = 0;
+
+    // Want the spacing to be 2/3 of the width of the bars
+    // maxLength = numOfBars * width + (numOfBars + 1) * spacingBetweenBars
+
+    const numOfBars = this.data.length;
+
+    const widthOfBars = this.maxLength / ((5 / 3) * numOfBars + 2 / 3);
+    const spacingBetweenBars = (2 / 3) * widthOfBars;
+    let basePosition = 0 + spacingBetweenBars;
+
+    const normalizedData = this.data.map((elem) => {
+      // console.log("ELEM FØR: ", elem)
+      elem = elem / this.normalizationFactor;
+      //   console.log("ELEM ETTER: ", elem);
+      return elem;
+    });
+
+    // console.log("Normalisert data: ", this.data);
+
+    while (counter < normalizedData.length) {
+      const height = normalizedData[counter];
+
+      const bar = new Polygon(
+        [
+          [basePosition, height],
+          [basePosition + widthOfBars, height],
+          [basePosition + widthOfBars, 0],
+          [basePosition, 0],
         ],
-        fontSize: fontSize,
-        anchorY: "middle",
-        anchorX: "left",
-      });
-      allLabels.add(valueOfValueLine);
-    }
+        { transparent: false, opacity: 1.0 }
+      );
 
-    lines.add(xLine);
-    lines.add(yLine);
-    xLine.setZIndex(10);
-    yLine.setZIndex(10);
-
-    if (this.diagramTitle) {
-      const title = new Text(this.diagramTitle, {
-        fontSize: fontSize + 8,
-        position: [basePosition / 2, yLineEndpoint[1] + 0.5],
+      const label = new Text(this.labels[counter], {
+        fontSize: this.fontSize,
+        position: [
+          basePosition + widthOfBars / 2,
+          this.labelsNextToLinePosition,
+        ],
         anchorX: "center",
       });
-      allLabels.add(title);
-    }
+      const valueOfBar = new Text("" + this.data[counter], {
+        fontSize: this.fontSize,
+        position: [basePosition + widthOfBars / 2, height + 0.1],
+        anchorX: "center",
+      });
+      basePosition += widthOfBars + spacingBetweenBars;
 
+      allBars.add(bar);
+      allBarsLabels.add(label);
+      allBarsLabels.add(valueOfBar);
+      counter++;
+    }
+    this.add(allBars);
+    this.add(allBarsLabels);
+    return basePosition;
+  }
+
+  addAxes(basePosition: number, stringLengthMultiplier: number) {
+    const axes = new Group();
+
+    const xLineCoord: Position = [
+      [0, 0],
+      [basePosition, 0],
+    ];
+    const yLineCoord: Position = [
+      [0, 0],
+      [0, this.maxHeight * 1.25],
+    ];
+
+    const xLine = new Line(xLineCoord[0], xLineCoord[1], { arrowhead: true });
+    const yLine = new Line(yLineCoord[0], yLineCoord[1], {
+      arrowhead: true,
+    });
+
+    const xAxisTitle = new Text(this.xAxisTitle, {
+      fontSize: this.fontSize,
+      position: [basePosition / 2, this.labelsNextToLinePosition * 2.5],
+      anchorX: "center",
+    });
+    const yAxisTitle = new Text(this.yAxisTitle, {
+      fontSize: this.fontSize,
+      position: [
+        this.labelsNextToLinePosition * 2.5 -
+          this.maxData.toString().length * stringLengthMultiplier,
+        yLineCoord[1][1] / 2,
+      ],
+      anchorX: "center",
+      anchorY: "top",
+    });
+    yAxisTitle.rotateZ(Math.PI / 2);
+
+    axes.add(xLine);
+    axes.add(xAxisTitle);
+    axes.add(yLine);
+    axes.add(yAxisTitle);
+
+    // Moves the axes in front of the horizontal lines
+    xLine.setZIndex(10);
+    yLine.setZIndex(10);
+    this.add(axes);
+    return [xLineCoord, yLineCoord];
+  }
+
+  addTitle(position: [number, number]) {
+    if (this.diagramTitle) {
+      const title = new Text(this.diagramTitle, {
+        fontSize: this.fontSize + 8,
+        position: [position[0] / 2, position[1] + 0.5],
+        anchorX: "center",
+      });
+      this.add(title);
+    }
+  }
+
+  addAxisUnits(xLineCoord: Position, yLineCoord: Position) {
     if (this.xAxisUnit) {
       const xAxisUnit = new Text(this.xAxisUnit, {
-        fontSize: fontSize,
-        position: [xLineEndpoint[0] + 0.1, 0],
+        fontSize: this.fontSize,
+        position: [xLineCoord[1][0] + 0.1, 0],
         anchorX: "left",
         anchorY: "middle",
       });
-      allLabels.add(xAxisUnit);
+      this.add(xAxisUnit);
     }
 
     if (this.yAxisUnit) {
       const yAxisUnit = new Text(this.yAxisUnit, {
-        fontSize: fontSize,
-        position: [0, yLineEndpoint[1]],
+        fontSize: this.fontSize,
+        position: [0, yLineCoord[1][1]],
         anchorX: "center",
         anchorY: "bottom",
       });
-      allLabels.add(yAxisUnit);
+      this.add(yAxisUnit);
     }
+  }
 
-    this.add(lines);
-    this.add(allBars);
-    this.add(allLabels);
+  addHorizontalLines(
+    numOfLines: number,
+    stringLengthMultiplier: number,
+    xLineCoord: Position,
+    yLineCoord: Position
+  ) {
+    const horizontalLines = new Group();
+    const lineLabels = new Group();
+    const gray = 0xaaaaaa;
+
+    for (let i = 1; i < numOfLines + 1; i++) {
+      const yCoord = (this.maxData / this.normalizationFactor / numOfLines) * i;
+      const valueLine = new Line([0, yCoord], [xLineCoord[1][0], yCoord], {
+        color: gray,
+      });
+      horizontalLines.add(valueLine);
+      const yValue = Math.round(yCoord * this.normalizationFactor);
+      const valueOfValueLine = new Text("" + yValue, {
+        position: [
+          this.labelsNextToLinePosition -
+            yValue.toString().length * stringLengthMultiplier,
+          yCoord,
+        ],
+        fontSize: this.fontSize,
+        anchorY: "middle",
+        anchorX: "left",
+      });
+      lineLabels.add(valueOfValueLine);
+      this.add(horizontalLines);
+      this.add(lineLabels);
+    }
   }
 
   //   // Function to calculate a nice interval
