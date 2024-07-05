2d022868fbe510034daa0d66703f7a094121a894
Jonas de Luna Skulberg <jonasdls@gmail.com>
2024-07-05 10:53:21 +0200

    Update legend box import

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ export { default as InfiniteLine } from "./Components/InfiniteLine";
 export { default as InputField } from "./Components/InputField";
 export { default as Label } from "./Components/Label";
 export { default as Latex } from "./Components/Latex";
-export { default as LegendBox } from "./Components/Latex";
+export { default as LegendBox } from "./Components/LegendBox";
 export { default as Line } from "./Components/Line";
 export { default as Plot } from "./Components/Plot";
 export { default as Point } from "./Components/Point";
